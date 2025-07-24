3aeb2afedf5942bdb209f61578fef9fe8d111f15
Gaytri Tripathi <gaytritripathi121@gmail.com>
2025-07-24 20:56:49 +0530

    Initial commit

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,17 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
-import path from "path";
+import path from 'path';
+import { fileURLToPath } from 'url';
 import listRoutes from './routes/listRoutes.js';
 import authRoutes from './routes/authRoutes.js';
 import taskRoutes from './routes/taskRoutes.js';
 
 dotenv.config();
 connectDB();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -18,15 +22,15 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
 app.use('/api/lists', listRoutes);
+
 if (process.env.NODE_ENV === "production") {
-  const frontendPath = path.join(__dirname, "../frontend/dist");
+  const frontendPath = path.join(__dirname, "../frontend/dist"); // or 'build' if that is your build folder
   app.use(express.static(frontendPath));
-  
+
   app.get(/.*/, (req, res) => {
     res.sendFile(path.join(frontendPath, "index.html"));
   });
 }
-
 
 app.use((err, req, res, next) => {
   console.error(`❌ Error at ${req.method} ${req.originalUrl}`, err);
@@ -40,7 +44,6 @@ app.use((err, req, res, next) => {
 });
 
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
